c7357e64968ae048eb3851d91beac740e3d65cf5
Philiphackerzx <65859306+Philiphackerzx@users.noreply.github.com>
2020-05-25 20:13:21 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-
-apt update && apt upgrade -y
-pkg install openssl-tool -y
-pkg install zip -y
-pkg install unzip -y
-pkg install gnupg -y
-termux-setup-storage
-mkdir "/sdcard/www.philiphacker.in"
-sh check.sh /data/data/com.termux/files/usr/bin/ngrok
