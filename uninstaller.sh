e8702d77f35b3ac64a252ab1ab0e605c890d792e
Philiphackerzx <65859306+Philiphackerzx@users.noreply.github.com>
2020-05-26 20:35:33 +0530

    Update uninstaller.sh

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,6 +1,6 @@
 rm -rf /data/data/com.termux/files/home/www.philiphacker.in
 rm /sdcard/htdocs/*
-cd /data/data/com.termux/files/home/
+cd /data/data/com.termux/files/home
 clear
 echo Uninstall Successful !
 read pause
