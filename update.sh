470376e586085a43034eb642a5d15893d84df853
Philiphackerzx <65859306+Philiphackerzx@users.noreply.github.com>
2020-05-26 19:45:49 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@ rm -rf test
 git clone https://github.com/Philiphackerzx/test.git
 file="test/version.io"
 update=$(cat "$file")
-if [ $update -eq 1 ]
+if [ $update -eq 2 ]
 then
 clear
 echo "No Updates Available right now !"
