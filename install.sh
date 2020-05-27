a8f078fa81ef389dd69d9afc3ded20fcece070fc
Philiphackerzx <65859306+Philiphackerzx@users.noreply.github.com>
2020-05-27 13:37:34 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,17 @@ echo "Downloading Requirements..."
 sleep 5
 apt update && apt upgrade -y
 clear
-echo Termux Updated !
+echo Termux Updated ! (1/4)
+sleep 2
+clear
 pkg install python2 -y
 sleep 2
 clear
-echo Python3 Installed !
+echo Python3 Installed ! (2/4)
 sleep 2
 pkg install openssl-tool -y
 clear
-echo SSL Installed !
+echo SSL Installed ! (3/4)
 sleep 2
 pkg install wget -y
 clear
@@ -33,7 +35,7 @@ echo "Do Not Delete this folder or file" > /sdcard/www.philiphacker.in/DoNotDele
 mkdir "/sdcard/htdocs"
 clear
 echo 1 > install.io
-echo Installation Done !
+echo Installation Done ! (4/4) Starting Tool..
 sleep 3
 sh start
 
